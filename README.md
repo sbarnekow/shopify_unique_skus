f6acd6a1ad80460e829b9368e2b625c5f4304c81
sbarnekow <mellamopepe@gmail.com>
2015-12-14 12:59:21 -0800

    added handling for nil and gear

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 takes shopify product export and parses doc for "Variant SKUs" to see if there are duplicates
 
 to use :
+
 1. add shopify product export to the "unique_skus" folder
+
 2. replace "products_export12_14.csv" with your export file name
+
 3. run `ruby csv_parse.rb`
--- csv_parse.rb
+++ csv_parse.rb
@@ -1,9 +1,82 @@
 require 'csv'
 
-skus = []
-CSV.foreach("products_export12_14.csv", headers: true) do |row|
-  skus.push(row["Variant SKU"])
+def find_duplicate_skus(file_name)
+  products = []
+  skus = []
+
+  products_with_dupe_skus_include_nil = []
+
+  CSV.foreach(file_name, headers: true) do |row|
+    products.push(row)
+    unless row["Variant SKU"].nil? || row["Variant SKU"] == ""
+      skus.push(row["Variant SKU"])
+    end
+  end
+
+  duplicate_skus =  skus.find_all{ |sku| skus.count(sku) > 1 }
+  unique_dupes = duplicate_skus.uniq.sort!
+
+  unique_dupes.each do |sku|
+    products.each do |product|
+      if product["Variant SKU"] == sku
+        products_with_dupe_skus_include_nil.push(product)
+      end
+    end
+  end
+
+  CSV.open("products_with_dupe_skus.csv", "wb") do |csv|
+    products_with_dupe_skus_include_nil.each do |product|
+      csv << product
+    end
+  end
 end
 
-duplicate_skus = skus.find_all{ |sku| skus.count(sku) > 1 }
-puts duplicate_skus
+def find_duplicate_skus_sans_gear(file_name)
+  products = []
+  skus = []
+
+  products_with_dupe_skus_include_nil = []
+
+  CSV.foreach(file_name, headers: true) do |row|
+    products.push(row)
+    unless row["Variant SKU"].nil? || row["Variant SKU"] == "" || row["Variant SKU"].upcase.include?("GEAR")
+      skus.push(row["Variant SKU"])
+    end
+  end
+
+  duplicate_skus =  skus.find_all{ |sku| skus.count(sku) > 1 }
+  unique_dupes = duplicate_skus.uniq.sort!
+
+  unique_dupes.each do |sku|
+    products.each do |product|
+      if product["Variant SKU"] == sku
+        products_with_dupe_skus_include_nil.push(product)
+      end
+    end
+  end
+
+  CSV.open("products_with_dupe_skus_sans_gear.csv", "wb") do |csv|
+    products_with_dupe_skus_include_nil.each do |product|
+      csv << product
+    end
+  end
+end
+
+
+def find_nil_skus(file_name)
+  nil_skus = []
+  CSV.foreach(file_name, headers: true) do |row|
+    if row["Variant SKU"].nil? || row["Variant SKU"] == ""
+      nil_skus.push(row)
+    end
+  end
+  CSV.open("products_with_nil_skus.csv", "wb") do |csv|
+    nil_skus.each do |product|
+      csv << product
+    end
+  end
+end
+
+find_duplicate_skus("products_export12_14.csv")
+find_duplicate_skus_sans_gear("products_export12_14.csv")
+# find_nil_skus("products_export12_14.csv")
